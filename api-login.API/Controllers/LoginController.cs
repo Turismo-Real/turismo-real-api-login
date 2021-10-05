7e304af2219548e3e1d42686eb06f2c2a338866b
Enzo Norese <enzo.norese@gmail.com>
2021-10-04 23:29:28 -0300

    Se agrega log

--- api-login.API/Controllers/LoginController.cs
+++ api-login.API/Controllers/LoginController.cs
@@ -2,11 +2,10 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using api_login.CORE.DTOs;
 using api_login.CORE.Interfaces;
+using api_login.CORE.Log;
 
 namespace api_login.API.Controllers
 {
@@ -24,6 +23,11 @@ namespace api_login.API.Controllers
         [HttpGet]
         public async Task<object> Login([FromBody]LoginPayload pyl)
         {
+            LogModel log = new LogModel();
+            log.servicio = "turismo-real-api-login";
+            log.payload = pyl;
+            DateTime startService = DateTime.Now;
+
             ResponseOK responseOK = new ResponseOK();
             bool result = await _loginRepository.LoginAsync(pyl.email, pyl.password);
             if (result)
@@ -36,6 +40,16 @@ namespace api_login.API.Controllers
                 responseOK.message = "Usuario No Autorizado.";
                 responseOK.login = result;
             }
+
+            // LOG
+            log.inicioSolicitud = startService;
+            log.finSolicitud = DateTime.Now;
+            log.tiempoSolicitud = (log.finSolicitud - log.inicioSolicitud).TotalMilliseconds + " ms";
+            log.statusCode = 200;
+            log.response = responseOK;
+            Console.WriteLine(log.parseJson());
+            // LOG
+
             return responseOK;
         }
     }
---
+++ api-login.CORE/Log/LogModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace api_login.CORE.Log
+{
+    public class LogModel
+    {
+        public string level = "INFO";
+        public int statusCode = 0;
+        public string servicio = string.Empty;
+        public DateTime inicioSolicitud;
+        public DateTime finSolicitud;
+        public string tiempoSolicitud;
+        public object payload = null;
+        public object response = null;
+
+        public string parseJson()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(this) + "\n";
+        }
+
+    }
+}
